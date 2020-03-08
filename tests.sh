ad4c03f664d82b2af6ac001e50f25be0965dfc1b
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-03-08 17:01:09 +0100

    tests.sh modified

--- tests.sh
+++ tests.sh
@@ -7,5 +7,7 @@ OOPS() { STDERR "$@"; exit 23; };
 TMP="$(mktemp -d)" || OOPS mktemp failed;
 trap 'rm -rf "$TMP"' 0	# cleanup
 
+[ -d "$1" ] || STDOUT skipping tests as "$1" does not exist || exit 0
+
 echo "Tests: Not yet implemented"
 
